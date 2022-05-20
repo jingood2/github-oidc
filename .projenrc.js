48cffb43055733a653641192d2f9309ea41e2026
jingood2 <jingood2@gmail.com>
2022-05-20 20:46:52 +0900

    update 9

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,12 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.24.1',
   defaultReleaseBranch: 'main',
   name: 'github-oidc',
 
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    'aws-cdk-github-oidc'
+  ],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
