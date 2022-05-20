9248f87db3ea44ca9f93f3358e4ce8d2c5477ed2
jingood2 <jingood2@gmail.com>
2022-05-20 18:55:23 +0900

    add new github-oidc-role

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { GithubActionsIdentityProvider, GithubActionsRole } from 'aws-cdk-github-oidc';
+import { App, Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // define resources here...
+    /**
+    * Create an Identity provider for GitHub inside your AWS Account. This
+    * allows GitHub to present itself to AWS IAM and assume a role.
+    */
+    const provider = new GithubActionsIdentityProvider(this, 'GithubProvider');
+
+    const deployRole = new GithubActionsRole(this, 'GithubDeployRole', {
+      provider: provider, // reference into the OIDC provider
+      owner: 'jingood2', // your repository owner (organization or user) name
+      repo: 'github-cdk-repo', // your repository name (without the owner name)
+      roleName: 'github-oidc-role',
+      description: 'This role deploys stuff to AWS',
+      maxSessionDuration: Duration.hours(2),
+    });
+
+    deployRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess'));
   }
 }
+
 
 // for development, use account/region from cdk cli
 const devEnv = {
@@ -17,7 +35,6 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'github-oidc-dev', { env: devEnv });
-// new MyStack(app, 'github-oidc-prod', { env: prodEnv });
+new MyStack(app, 'github-oidc', { env: devEnv });
 
 app.synth();
